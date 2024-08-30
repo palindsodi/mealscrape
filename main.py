667df054e099713687fae6564cbc3e2eb3e2fc27
palindromic sodium <129000615+palindsodi@users.noreply.github.com>
2024-08-29 19:55:01 -0700

    relocalize

--- main.py
+++ main.py
@@ -5,17 +5,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import StaleElementReferenceException
 import time
 import random
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service as ChromeService
-import pandas as pd
-import streamlit as st
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--window-size=1920x1080')
-chrome_options.add_argument('--disable-gpu')
-
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+driver = webdriver.Chrome()
 wait = WebDriverWait(driver, 10)
 url = "https://rdeapps.stanford.edu/dininghallmenu/"
 driver.get(url)
@@ -37,26 +28,26 @@ hall_dropdown = driver.find_element(By.ID, "MainContent_lstLocations")
 # list names of halls
 halls = [h.get_attribute("value") for h in hall_dropdown.find_elements(By.TAG_NAME, "option") if h.get_attribute("value") != '']
 for hall in halls:
-    st.write(hall)
+    print(hall)
     selector("MainContent_lstLocations", hall)
 
     day_dropdown = driver.find_element(By.ID, "MainContent_lstDay")
     # list names of days
     days = [d.get_attribute("value") for d in day_dropdown.find_elements(By.TAG_NAME, "option") if d.get_attribute("value") != '']
     for day in days:
-        st.write(day)
+        print(day)
         selector("MainContent_lstDay", day)
 
         meal_dropdown = driver.find_element(By.ID, "MainContent_lstMealType")
         # list names of meals
         meals = [m.get_attribute("value") for m in meal_dropdown.find_elements(By.TAG_NAME, "option") if m.get_attribute("value") != '']
         for meal in meals:
-            st.write(meal)
+            print(meal)
             selector("MainContent_lstMealType", meal)
 
             # collect food
             food = driver.find_elements(By.CSS_SELECTOR, "div[class^='clsMenuItem']")
             if food:
-                st.write([f.text for f in food])
+                print([f.text for f in food])
 
 driver.quit()
