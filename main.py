908de00f850fb3855d1652f456b7d0ebd771b7ca
palindromic sodium <anna.aq.wu@gmail.com>
2024-08-29 22:26:09 -0700

    beautified output

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ url = "https://rdeapps.stanford.edu/dininghallmenu/"
 driver.get(url)
 
 def selector(element_id, option_value):
-    time.sleep(random.randint(0.5,2)*0.75)
+    time.sleep(random.randint(100,200)*0.01+1)
     dropdown = wait.until(EC.presence_of_element_located((By.ID, element_id)))
     for option in dropdown.find_elements(By.TAG_NAME, "option"):
         # find the matching name in dropdown 
@@ -28,26 +28,35 @@ hall_dropdown = driver.find_element(By.ID, "MainContent_lstLocations")
 # list names of halls
 halls = [h.get_attribute("value") for h in hall_dropdown.find_elements(By.TAG_NAME, "option") if h.get_attribute("value") != '']
 for hall in halls:
-    print(hall)
+    print("|||||||| DINING HALL: " + hall + " ||||||||")
     selector("MainContent_lstLocations", hall)
 
     day_dropdown = driver.find_element(By.ID, "MainContent_lstDay")
     # list names of days
     days = [d.get_attribute("value") for d in day_dropdown.find_elements(By.TAG_NAME, "option") if d.get_attribute("value") != '']
     for day in days:
-        print(day)
+        print("\t• " + day)
         selector("MainContent_lstDay", day)
 
         meal_dropdown = driver.find_element(By.ID, "MainContent_lstMealType")
         # list names of meals
         meals = [m.get_attribute("value") for m in meal_dropdown.find_elements(By.TAG_NAME, "option") if m.get_attribute("value") != '']
         for meal in meals:
-            print(meal)
+            print("\t\t------- " + ''.join(meal).upper() + " -------")
             selector("MainContent_lstMealType", meal)
 
             # collect food
-            food = driver.find_elements(By.CSS_SELECTOR, "div[class^='clsMenuItem']")
-            if food:
-                print([f.text for f in food])
-
+            foods = driver.find_elements(By.CSS_SELECTOR, "div[class^='clsMenuItem']")
+            if foods:
+                for food in foods: 
+                    food_text = food.text.replace("\n", "\n\t\t\t")
+                    print('\t\t\t• ' + ''.join(food_text) + '\n')
+                    for category in food.find_elements(By.TAG_NAME, "img"):
+                        image_string = category.get_attribute("src")
+                        if "png" in image_string:
+                            if "H" in image_string: print("\t\t\tHALAL\n")
+                            if "K" in image_string: print("\t\t\tKOSHER\n")
+                            if "GF" in image_string: print("\t\t\tGLUTEN FREE\n")
+                            if "VGN" in image_string: print("\t\t\tVEGAN\n")
+                            elif "V" in image_string: print("\t\t\tVEGETARIAN\n")
 driver.quit()
